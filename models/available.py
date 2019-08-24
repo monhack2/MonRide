7f2012ed70177446eb9122327efc5a4f619bc850
Mariusz Skoneczko <mariusz@skoneczko.com>
2019-08-25 00:06:47 +1000

    Availability doesnt error now

--- models/available.py
+++ models/available.py
@@ -3,4 +3,5 @@ from monride import db
 
 class Available(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    userId = db.Column(db.Integer, backref=db.backref('users', lazy=True))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship('User', backref=db.backref('user', lazy=True))
