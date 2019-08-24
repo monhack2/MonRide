b189baa4f2295ae87b51a3dc28b58bff4a9d25fa
Mariusz Skoneczko <mariusz@skoneczko.com>
2019-08-24 22:19:15 +1000

    Fix typo

--- models/destination.py
+++ models/destination.py
@@ -6,4 +6,4 @@ class Destination(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     address = db.Column(db.String, nullable=False)
-    postcode = db.Column(db.Intger, nullable=False)
+    postcode = db.Column(db.Integer, nullable=False)
