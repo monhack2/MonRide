b5b15bcb05054d90cab658451ab9ecde04c93b5d
Salang Kang <skan0026@student.monash.edu>
2019-08-24 17:12:09 +1000

    db stuff v2

--- models/user.py
+++ models/user.py
@@ -5,9 +5,8 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True, nullable=False)
     email = db.Column(db.String, unique=True, nullable=False)
+    first_name = db.Column(db.String, unique=True, nullable=True)
+    last_name = db.Column(db.String, unique=True, nullable=True)
     driver = db.Column(db.Boolean)
     rating = db.Column(db.Float)
-    first_name = db.Column(db.String, unique=True, nullable=False)
-    last_name = db.Column(db.String, unique=True, nullable=False)
-
 
