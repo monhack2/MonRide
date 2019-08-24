d8f4931ab581ae519fee1620dab2ccbbe22b4021
Mariusz Skoneczko <mariusz@skoneczko.com>
2019-08-24 21:37:05 +1000

    Improve db usage

--- monride.py
+++ monride.py
@@ -2,13 +2,18 @@ from flask import Flask
 from flask import render_template
 from flask_sqlalchemy import SQLAlchemy
 
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
+
+from models.user import User
+doge = User.query.filter_by(username='doge').first()
+
 @app.route('/')
 def hello_world():
-    return render_template('index.html', current='home')
+    return render_template('index.html', current='home', username=doge.username, stars=doge.rating)
 
 
 @app.route('/book')
