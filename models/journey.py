4364feb46ee27f5cb9cade8b49cd0d307e8ea6d6
Salang Kang <skan0026@student.monash.edu>
2019-08-25 00:01:32 +1000

    fixed passenger

--- models/journey.py
+++ models/journey.py
@@ -3,4 +3,4 @@ from monride import db
 class Journey(db.Model):
     __tablename__ = 'journey'
     id = db.Column(db.Integer, primary_key=True)
-    passengers = db.relationship('User', backref='passenger', lazy=True)
+    passengers = db.relationship('User', backref='user', lazy=True)
